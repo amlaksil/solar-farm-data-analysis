cae3175db797a2281f639a0e73b930f6cc2ecce2
amlaksil <silamlakdesye@gmail.com>
2024-04-24 08:53:53 +0300

    Implement function that calculates statistical measures for each numeric column in a dataset

---
+++ scripts/summary_statistics.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+"""
+Module to calculate statistical measures for each numeric column in a dataset.
+
+To calculate the mean, median, standard deviation, and other statistical
+measures for each numeric column in the dataset to understand the data
+distribution, we can use the describe() method in Pandas.
+"""
+import pandas as pd
+import os
+import sys
+
+
+def calculate_statistical_measures(path):
+    """
+    Calculate and display the mean, median, standard deviation, minimum,
+    maximum, and quartile values for each numeric column in the dataset.
+
+    Args:
+        path (str): The path to the CSV file containing the dataset.
+
+    Raises:
+        FileNotFoundError: If the specified file path does not exist.
+
+    Example:
+        calculate_statistical_measures('data.csv')
+    """
+    if os.path.exists(path):
+        # Load the data
+        data = pd.read_csv(path)
+        numeric_stats = data.describe()
+        print(numeric_stats)
+    else:
+        raise FileNotFoundError("File not found")
+
+
+if __name__ == '__main__':
+    if len(sys.argv) > 1:
+        calculate_statistical_measures(sys.argv[1])
